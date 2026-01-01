d4f5117914598c1b84d595e5a5c7ea53b1e2b5ae
unknown <asifazad>
2026-01-01 12:33:18 +0530

    contact form issue fixed completely

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -3,10 +3,18 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
     const data = await req.json();
 
+    const formBody = new URLSearchParams();
+    formBody.append("name", data.name);
+    formBody.append("email", data.email);
+    formBody.append("mobile", data.mobile);
+    formBody.append("message", data.message);
+
+
     await fetch('https://script.google.com/macros/s/AKfycbzdOvQz_3S36dIrSwI1muUU5CvIMrdDV3TKLbK8pGwzk5bfPlutZL3SBwm8yKAq3FEl/exec', {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data),
+        headers: { "Content-Type": "application/x-www-form-urlencoded", },
+
+        body: formBody.toString()
     });
 
     return NextResponse.json({ success: true });
